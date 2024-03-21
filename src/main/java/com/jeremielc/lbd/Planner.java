ce9e689de7f9f88b2a36f3288c06d16ea1d69c1c
LECLERC, Jeremie <jeremie.leclerc@sap.com>
2024-03-21 17:15:26 +0100

    Init planner.

---
+++ src/main/java/com/jeremielc/lbd/Planner.java
@@ -0,0 +1,18 @@
+package com.jeremielc.lbd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jeremielc.lbd.pojo.MatchSet;
+import com.jeremielc.lbd.pojo.TournamentConfig;
+
+public class Planner {
+    public static List<MatchSet> plan(TournamentConfig config, int courtCount) {
+        List<MatchSet> rounds = new ArrayList<>();
+        MatchSet round = new MatchSet();
+
+        
+
+        return rounds;
+    }
+}
---
+++ src/main/java/com/jeremielc/lbd/pojo/MatchSet.java
@@ -0,0 +1,41 @@
+package com.jeremielc.lbd.pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jeremielc.lbd.pojo.match.AbstractMatch;
+
+public class MatchSet implements Comparable<MatchSet> {
+    private final List<AbstractMatch> matchList;
+
+    public MatchSet() {
+        this.matchList = new ArrayList<>();
+    }
+
+    public MatchSet(AbstractMatch... args) {
+        this.matchList = new ArrayList<>();
+
+        for (AbstractMatch match : args) {
+            this.matchList.add(match);
+        }
+    }
+
+    public void addMatch(AbstractMatch match) {
+        matchList.add(match);
+    }
+
+    @Override
+    public int compareTo(MatchSet o) {
+        if (this.matchList.size() == o.getMatchList().size()) {
+            return 0;
+        } else if (this.matchList.size() > o.getMatchList().size()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    public List<AbstractMatch> getMatchList() {
+        return this.getMatchList();
+    }
+}
